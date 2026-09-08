256bd56f234ea4d77e52363c4a4c46f55ec7ab05
guimeida1950-droid <guimeida1950@gmail.com>
2026-09-08 18:54:39 -0300

    Adiciona configuracoes funcionais e atualiza modulos

--- Conexao.java
+++ Conexao.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 
 public class Conexao {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/clinix";
+    private static final String URL = ""minhaSenhaReal";
     private static final String USUARIO = "root";
 
     private static final String SENHA = "root";
